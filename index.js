06e67e747161fe0674d47cd5ca66def1faf57e1e
Nataly505 <dmitrievan730@gmail.com>
2025-03-04 15:50:00 +0500

    добавили обработку команды get-todos

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ readLine(processCommand);
 
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    return filePaths.map(path => readFile(path));
+    return filePaths.map(path => ({
+        path: path,
+        content: readFile(path)
+    }));
 }
 
 function processCommand(command) {
@@ -16,10 +19,25 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'get-todos':
+            getTodos();
+            break;
         default:
             console.log('wrong command');
             break;
     }
 }
 
-// TODO you can do it!
+function getTodos() {
+    const todos = files.flatMap(file => {
+        const matches = file.content.match(/\/\/ TODO .*/g) || [];
+        return matches.map(match => `${file.path}: ${match}`);
+    });
+
+    if (todos.length > 0) {
+        console.log('Найдены следующие TODO комментарии:');
+        todos.forEach(todo => console.log(todo));
+    } else {
+        console.log('TODO комментарии не найдены');
+    }
+}

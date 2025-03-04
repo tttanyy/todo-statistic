9e83a4582c330f688690a2236e1e55e8652ff48a
Tatiana Gugina <Tatiana.Gugina@urfu.me>
2025-03-04 16:23:49 +0500

    добавлена обработка команды user...

--- index.js
+++ index.js
@@ -15,18 +15,27 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command.trim()) {
+    const [cmd, ...args] = command.trim().split(' ');
+
+    switch (cmd) {
         case 'exit':
             process.exit(0);
             break;
-        case 'get-todos': 
+        case 'get-todos':
             console.log(getTodos());
             break;
-        case 'show': 
+        case 'show':
             showTodos();
             break;
-        case 'important': 
+        case 'important':
             showImportantTodos();
+            break;
+        case 'user':
+            if (args.length === 1) {
+                showUserTodos(args[0].toLowerCase()); 
+            } else {
+                console.log('Usage: user {username}');
+            }
             break;
         default:
             console.log('wrong command');
@@ -46,21 +55,43 @@ function showTodos() {
     const todos = getTodos();
 
     if (todos.length > 0) {
-        console.log('TODO comments:');
-        todos.forEach(todo => console.log(todo)); 
+        console.log('List of TODO comments:');
+        todos.forEach(todo => console.log(todo));
     } else {
-        console.log('No TODO comments found');
+        console.log('No TODO comments found.');
     }
 }
 
 function showImportantTodos() {
-    const todos = getTodos(); 
-    const importantTodos = todos.filter(todo => todo.includes('!')); 
+    const todos = getTodos();
+    const importantTodos = todos.filter(todo => todo.includes('!'));
 
     if (importantTodos.length > 0) {
-        console.log('important TODO comments:');
+        console.log('List of important TODO comments:');
         importantTodos.forEach(todo => console.log(todo));
     } else {
-        console.log('No important TODO comments found');
+        console.log('No important TODO comments found.');
+    }
+}
+
+function showUserTodos(username) {
+    const todos = getTodos();
+    const userTodos = todos.filter(todo => {
+        const match = todo.match(/\/\/ TODO\s*({[^;]+}|[^;]+?);/);
+        if (match) {
+            let extractedUsername = match[1].trim();
+            if (extractedUsername.startsWith('{') && extractedUsername.endsWith('}')) {
+                extractedUsername = extractedUsername.slice(1, -1).trim();
+            }
+            return extractedUsername.toLowerCase() === username; 
+        }
+        return false;
+    });
+
+    if (userTodos.length > 0) {
+        console.log(`List of TODO comments by user "${username}":`);
+        userTodos.forEach(todo => console.log(todo));
+    } else {
+        console.log(`No TODO comments found for user "${username}".`);
     }
 }
